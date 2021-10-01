1f4dc67e7cad73c38f0404a6df1d944cbb2615bc
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-10-01 18:09:15 +0300

    düzeltildi.

---
+++ HTML-CSS-JS/Dersler/30Eylül21/app.js
@@ -0,0 +1,20 @@
+console.log("I'm not outside");
+var a = 10
+console.log('a baslangıc :' ,a);
+// {
+//     let b = 5;
+//     console.log('scope icinde b:' ,b);
+// }
+{
+    var b = 5;
+    console.log('scope icinde b:' ,b);
+}
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+var c = 23;
+console.log(c);
+console.log(d);
+let d = 15;

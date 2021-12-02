385c21a5457ce7be2c63986cf26b59ae455ca7ea
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-02 22:46:00 +0300

    last commit.

--- React/myapp/src/App.js
+++ React/myapp/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Age: {age}</h1>
+        {/* <h1>Age: {age}</h1> */}
         <ClassComponent old={age} car = "BMW"/>
         <FuncComp old={age+age} color = "yellow"/>
         {/* <p>

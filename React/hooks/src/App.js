2813bbff47bb39c35869df06cc185c86393b046c
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-07 16:03:26 +0300

    fourth commit

--- React/hooks/src/App.js
+++ React/hooks/src/App.js
@@ -1,8 +1,7 @@
 /** @format */
 
 import EmployeeList from "./components/EmployeeList";
-import EmployeeContextProvider, { EmployeeContext,
-} from "./contexts/EmployeeContext";
+import EmployeeContextProvider from "./contexts/EmployeeContext";
 
 function App() {
   return (

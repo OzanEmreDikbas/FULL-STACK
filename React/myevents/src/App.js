83847b1690beee5e891672cfebf062a761b0e89e
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-08 12:12:18 +0300

    last commit.

--- React/myevents/src/App.js
+++ React/myevents/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import AddEventsClass from './components/addEventClass/AddEventsClass';
 import AddEvents from './components/addEvents/AddEvents';
 import EventBind from './components/eventBinding/EventBind';
+import Form from './components/form/Form';
+import KeyboardEvents from './components/keyboardEvents/KeyboardEvents';
 import MouseEvents from './components/mouseEvents/MouseEvents';
 
 function App() {
@@ -11,7 +13,9 @@ function App() {
       {/* <AddEvents /> */}
       {/* <AddEventsClass /> */}
       {/* <EventBind /> */}
-      <MouseEvents />
+      {/* <MouseEvents /> */}
+      {/* <Form /> */}
+      <KeyboardEvents />
     </div>
   );
 }

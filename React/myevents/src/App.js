09d5f9314925eaa0dbfe16f65c5e437c5789aa9a
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-08 14:05:01 +0300

    changed.

--- React/myevents/src/App.js
+++ React/myevents/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-
+// import AddEventsClass from './components/addEventClass/AddEventsClass';
+// import AddEvents from './components/addEvents/AddEvents';
+// import EventBind from './components/eventBinding/EventBind';
+// import Form from './components/form/Form';
+// import KeyboardEvents from './components/keyboardEvents/KeyboardEvents';
 import MouseEvents from './components/mouseEvents/MouseEvents';
 
 function App() {
   return (
     <div className="App">
       <h1>Events in React Js</h1>
-
+      {/* <AddEvents /> */}
+      {/* <AddEventsClass /> */}
+      {/* <EventBind /> */}
       <MouseEvents />
-
+      {/* <Form /> */}
+      {/* <KeyboardEvents /> */}
     </div>
   );
 }

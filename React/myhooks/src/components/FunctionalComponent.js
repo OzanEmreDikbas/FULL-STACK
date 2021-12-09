25590017eb96636b1cc74f8a2d2edf321381d157
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-09 21:33:49 +0300

    third commit.

--- React/myhooks/src/components/FunctionalComponent.js
+++ React/myhooks/src/components/FunctionalComponent.js
@@ -1,33 +1,34 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FunctionalComponent = () => {
-    const [count, setCount] = useState(0)
-    const [age, setAge] =useState(4)
-    const [user, setUser] =useState({name: "Felix", age: 25, email: "felix@gmail.com"})
+  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(4)
+  // const [user, setUser] = useState({ name: 'Felix', age: 25, email: 'felix@gmail.com' })
 
-    // const increase = () => {
-    //     setCount(count + 1)
-    // }
-    const increase = () => setCount(count + 1)
+  useEffect(() => {
+    // count !== 0 && alert(`new notification, total: ${count} `)
+    console.log('UseEffect')
+  }, [count, age])
 
-    const increaseAge  = () => setAge(age + 1)
+  const increase = () => setCount(count + 1)
 
-    const updateUser = () => {
-        setUser({...user, name: "John"})
-    }
-
+  const increaseAge = () => setAge(age + 1)
+  // const updateUser = () => {
+  //   setUser({ ...user, name: 'John' })
+  // }
+  console.log('Func Comp Rendered')
   return (
     <div className="function">
       <h2>Functional Component</h2>
       <p>Count: {count}</p>
-      {/* <button onClick={() => setCount(count + 1)}>increase</button> */}
-      <button onClick={increase}>increase</button>      
-      <p>Age: {age}</p>
-      <button onClick={increaseAge}>increase</button>
-      <p>Name: {user.name}</p>
+      {/* <button onClick={() => setCount(count + 1)}>Increase</button> */}
+      <button onClick={increase}>Increase</button>
+      <p>Age : {age}</p>
+      <button onClick={increaseAge}>Increase Age</button>
+      {/* <p>Name: {user.name} </p>
       <p>Age: {user.age}</p>
-      <p>Email.: {user.email}</p>
-      <button onClick={updateUser}>UpdateUser</button>
+      <p>Email: {user.email}</p>
+      <button onClick={updateUser}>UpdateUser</button> */}
     </div>
   )
 }

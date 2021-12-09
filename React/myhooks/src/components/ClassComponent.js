cbbfee1001c6c04913e0960d0d5179887105fa0e
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-09 23:18:49 +0300

    last commit.

--- React/myhooks/src/components/ClassComponent.js
+++ React/myhooks/src/components/ClassComponent.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import React from 'react'
-
 class ClassComponent extends React.Component {
   constructor() {
     super()
--- React/myhooks/src/components/Users.js
+++ React/myhooks/src/components/Users.js
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 const Users = () => {
+  const [users, setUsers] = useState([])
+  // const getUsers = () => {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((res) => res.json())
+  //     .then((json) => setUsers(json))
+  //     .catch((err) => console.log(err))
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/users')
+  //     .then((res) => setUsers(res.data))
+  //     .catch((err) => console.log(err))
+  // }
+  useEffect(() => {
+    // axios
+    //   .get('https://jsonplaceholder.typicode.com/users')
+    //   .then((res) => setUsers(res.data))
+    //   .catch((err) => console.log(err))
+    axios('https://jsonplaceholder.typicode.com/users')
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+  console.log(users)
   return (
     <div className="users">
       <h2>Users</h2>
+      {/* <button onClick={getUsers}>Get Users</button> */}
+      <div className="users-cards">
+        {users.map((user) => (
+          <div className="useritem" key={user.id}>
+            <img src={`https://avatars.dicebear.com/v2/avataaars/${user.id}.svg`} alt="" />
+            {user.name}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
-
 export default Users

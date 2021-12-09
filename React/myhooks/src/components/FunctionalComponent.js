90b1b28d039dc6dbb4b908fb5a9c200503aed0f1
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-09 20:46:36 +0300

    first commit.

---
+++ React/myhooks/src/components/FunctionalComponent.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+
+const FunctionalComponent = () => {
+    const [count, setCount] = useState(0)
+    const [age, setAge] =useState(4)
+    const [user, setUser] =useState({name: "Felix", age: 25, email: "felix@gmail.com"})
+
+    // const increase = () => {
+    //     setCount(count + 1)
+    // }
+    const increase = () => setCount(count + 1)
+
+    const increaseAge  = () => setAge(age + 1)
+
+    const updateUser = () => {
+        setUser({...user, name: "John"})
+    }
+
+  return (
+    <div className="function">
+      <h2>Functional Component</h2>
+      <p>Count: {count}</p>
+      {/* <button onClick={() => setCount(count + 1)}>increase</button> */}
+      <button onClick={increase}>increase</button>      
+      <p>Age: {age}</p>
+      <button onClick={increaseAge}>increase</button>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <p>Email.: {user.email}</p>
+      <button onClick={updateUser}>UpdateUser</button>
+    </div>
+  )
+}
+
+export default FunctionalComponent

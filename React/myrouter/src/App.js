ad9a2b10d610904748802d865d368b6651a24357
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-11 15:48:01 +0300

    1. commit.

--- React/myrouter/src/App.js
+++ React/myrouter/src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
       <h1>Clarusway</h1>
+      <main>
+        <Routes>
+          <Route path='/' element={<Welcome />}/>
+          <Route path='/products' element={<Products />}/>
+        </Routes>
+      </main>
     </div>
   );
 }

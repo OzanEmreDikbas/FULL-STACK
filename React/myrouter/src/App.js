0c7abe2eaf373a3164a4c496c68b0a25fc0335f1
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-11 16:23:59 +0300

    3. commit.

--- React/myrouter/src/App.js
+++ React/myrouter/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Welcome />}/>
           <Route path='/products' element={<Products />}/>
+          <Route path='/productDetail' element={<ProductDetail />}/>
         </Routes>
       </main>
     </div>
---
+++ React/myrouter/src/component/MainHeader.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const MainHeader = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink className={(navData) => navData.isActive ? 'active' : ''} to='/welcome'>
+                            Welcome
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={(navData) => navData.isActive ? 'active' : ''} to='/products'>
+                            Products
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={(navData) => navData.isActive ? 'active' : ''} to='/productDetail'>
+                            Product Detail
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainHeader

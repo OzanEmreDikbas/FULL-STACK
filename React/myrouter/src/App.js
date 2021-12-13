1566d6f7d411e1b966fec6da8cb4bf2c1b9f5784
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-13 19:07:45 +0300

    first commit.

--- React/myrouter/src/App.js
+++ React/myrouter/src/App.js
@@ -4,14 +4,17 @@ import {Routes, Route} from 'react-router-dom'
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
+import MainHeader from './component/MainHeader';
 
 function App() {
   return (
     <div className="App">
       <h1>Clarusway</h1>
+      <MainHeader />
       <main>
         <Routes>
           <Route path='/' element={<Welcome />}/>
+          <Route path='/welcome' element={<Welcome />}/>
           <Route path='/products' element={<Products />}/>
           <Route path='/productDetail' element={<ProductDetail />}/>
         </Routes>
